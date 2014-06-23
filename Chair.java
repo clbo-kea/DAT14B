37e4734509bdc67b5b6d57c82f0d2d9f2a47d40f
Claus Bové <clbo@kea.dk>
2014-06-23 21:42:45 +0200

    chair added

---
+++ Chair.java
@@ -0,0 +1,31 @@
+public class Chair
+{
+ 
+   public int legs = 4;
+   public String color = "red";
+   public int rotation;
+   
+   // Constructor
+   public Chair(int rotation)
+   {
+      this.rotation = rotation;
+   }
+ 
+   public void turn()
+   {
+      System.out.print("The " + color + " Chair with ");
+      System.out.println(legs + " legs is rotating: ");
+      
+      for(int i = 1; i <= rotation; i++)
+      {
+         System.out.print(i + " ");
+      }
+      
+      System.out.println(" times!");
+   }
+   public void setColor(String color)
+   {
+      this.color = color;
+   }
+   
+}
